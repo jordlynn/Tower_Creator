579644a12fe669e4c103b5c17b1e314937c42b54
jordlynn <snake.river52@gmail.com>
2013-08-30 00:45:18 -0700

    More devleopement in getting grid working, just testing to do and it should be good

--- Editor/TowerAnimator/Grid.cs
+++ Editor/TowerAnimator/Grid.cs
@@ -1,4 +1,19 @@
-﻿using System.Collections.Generic;
+﻿/* Grid.cs
+				===== READ THIS! ====
+
+	Originally written by I'm assuming Josh Armstrong, now maintained by Jordan Lynn.
+this is currently in the works of being ported over the a *.tan file creator for the Kibbie
+Dome light project. When I (Jordan Lynn) adopted this code there wasn't any comments inside
+so anyting you see commented you can assume is accurate/up to date and should be followed.
+DO NOT use this code for the tower light show, in it's current state it will not work. It's
+been changed over to Kibbie Dome code for testing and that's our current most pressing matter.
+I did comment where I made changes and how to change them back if you ever need to for the
+full on tower lights show. IF YOU make any changes COMMENT explaining what you did, what it was,
+how to change it back, and your reason for changing. Ultimatly this code should be built to handle
+both cases but until that time we'll make due with this. Thanks!
+*/
+
+using System.Collections.Generic;
 using System.Drawing;
 
 namespace TowerAnimator
@@ -11,17 +26,31 @@ namespace TowerAnimator
 
         public Grid(int rows, int columns)
         {
-            this.rows = 1;
-            this.columns = 2;
+            this.rows = 12; // Hardcoding this in for Kibbie lights adjust back to "rows" as needed.
+            this.columns = 8; // same here adjust back to "columns" as needed.
 
             states = new List<List<Color>>();
+	/* Commenting this out it is currently tower lights code and will build a solid grid
+	    we need one with a hollow middle for the kibbie lights.
             for (int row = 0; row < rows; ++row)
             {
                 states.Add(new List<Color>());
                 for (int col = 0; col < columns; ++col)
                     states[row].Add(Color.Black);
             }
-        }
+       */
+			// Here I'm working on getting just a border for the grid instead of a whole rectangle,
+			// I achomplish this by going through each row and col but only giving values to the 
+			// ones that are either == to the begining row && col or end row && col.
+			for( int row = 0; row < rows; ++row ){ 
+				for( int col = 0; col < columns; ++col ){ 
+					if( row == 1 || r == rows || col == 1 || col == columns){
+						states.Add(new List<Color>());
+						states[row].Add(Color.Black)); 
+					}
+				}
+			}
+	}
 
         public Grid(Grid other)
         {
